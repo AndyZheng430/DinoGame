a4ad8a4f831990d4f4baa5a15bb54dc2869d11be
zhenga6 <zhenga6@bxscience.edu>
2018-11-20 23:40:43 -0500

    Working on Character movement
    Walking up in the z works with animation

--- DinoGame/Assets/Scripts/DinoController.cs
+++ DinoGame/Assets/Scripts/DinoController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DinoController : MonoBehaviour {
+    Animator anim;
+    public float speed = 10;
 
 	// Use this for initialization
 	void Start () {
-		
+        anim = this.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float walkSpeed = Input.GetAxis("Vertical") *speed;
+        anim.SetFloat("Speed", walkSpeed);
+        transform.Translate(0, 0, walkSpeed * Time.deltaTime);
 	}
 }

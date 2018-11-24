77cd25678122770350e21993eaf32361504f042f
zhenga6 <zhenga6@bxscience.edu>
2018-11-23 20:05:03 -0500

    Dino moving in all direction
    everything works fine

--- DinoGame/Assets/Scripts/DinoController.cs
+++ DinoGame/Assets/Scripts/DinoController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DinoController : MonoBehaviour {
     Animator anim;
-    public float speed = 5;
+    public float speed = 2;
 
 	// Use this for initialization
 	void Start () {
@@ -18,20 +18,39 @@ public class DinoController : MonoBehaviour {
         if (currentZSpeed == 0 && currentXSpeed == 0) {
             anim.SetFloat("Speed", 0);
         }
-        if (currentZSpeed > 0) {
-            if (!(this.transform.rotation.eulerAngles == new Vector3(0, 0, 0))) {
+        if (currentZSpeed > 0)
+        {
+            if (!(this.transform.rotation.eulerAngles == new Vector3(0, 0, 0)))
+            {
                 this.transform.rotation = Quaternion.identity;
             }
             anim.SetFloat("Speed", currentZSpeed * speed);
+            transform.Translate(new Vector3(0, 0, currentZSpeed));
         }
-        if (currentZSpeed < 0)
+        else if (currentZSpeed < 0)
         {
             if (!(this.transform.rotation.eulerAngles == new Vector3(0, 180, 0)))
             {
-                this.transform.Rotate(new Vector3(0, this.transform.rotation.eulerAngles.y + 180, 0));
+                this.transform.rotation = Quaternion.Euler(0, 180, 0);
             }
             anim.SetFloat("Speed", -currentZSpeed * speed);
+            transform.Translate(new Vector3(0, 0, -currentZSpeed));
         }
-        transform.Translate(new Vector3(0, 0, currentXSpeed));
+        else if (currentXSpeed > 0) {
+            if (!(this.transform.rotation.eulerAngles == new Vector3(0, 90, 0))) {
+                this.transform.rotation = Quaternion.Euler(0, 90, 0);
+            }
+            anim.SetFloat("Speed", currentXSpeed);
+            transform.Translate(new Vector3(0, 0, currentXSpeed));
+        }
+        else if (currentXSpeed < 0)
+        {
+            if (!(this.transform.rotation.eulerAngles == new Vector3(0, 270, 0)))
+            {
+                this.transform.rotation = Quaternion.Euler(0, 270, 0);
+            }
+            anim.SetFloat("Speed", -currentXSpeed);
+            transform.Translate(new Vector3(0, 0, -currentXSpeed));
+        }
     }
 }

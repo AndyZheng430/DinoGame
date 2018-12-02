567d47c1b123252628aef09c1787f9109df020ad
zhenga6 <zhenga6@bxscience.edu>
2018-12-01 20:22:28 -0500

    working on colliders

--- DinoGame/Assets/Scripts/CamControl.cs
+++ DinoGame/Assets/Scripts/CamControl.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CamControl : MonoBehaviour {
-
+    public GameObject player;
+    Vector3 offset;
 	// Use this for initialization
 	void Start () {
-		
+        offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.position = player.transform.position + offset;
 	}
 }
